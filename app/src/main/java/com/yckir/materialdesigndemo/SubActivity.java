845922a01dd38816645f77c64683f26935527db4
Ricky Martinez <crosscountry68@gmail.com>
2015-07-29 12:06:11 -0700

    slidenerd tutorial video 11
    end navigation drawer

---
+++ app/src/main/java/com/yckir/materialdesigndemo/SubActivity.java
@@ -0,0 +1,54 @@
+package com.yckir.materialdesigndemo;
+
+import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+
+public class SubActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sub);
+        Toolbar toolbar=(Toolbar)findViewById(R.id.app_bar);
+        setSupportActionBar(toolbar);
+        //enable up navigation button
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        Log.v("SubActivity", "first is null");
+        getSupportActionBar().setHomeButtonEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_sub, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        Log.v("SubActivity", "click");
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        if(id==android.R.id.home){
+            NavUtils.navigateUpFromSameTask(this);
+        }
+
+
+        return super.onOptionsItemSelected(item);
+    }
+}
